9fbd6820d56018f2a8c9416081cc0ca6cfadb585
j2d6 <j2d6.pro@gmail.com>
2024-10-16 17:08:33 +0300

    make coursItem => Link

--- components/cours/CoursListItem.tsx
+++ components/cours/CoursListItem.tsx
@@ -1,5 +1,6 @@
 import { getDayFromTimestamp, getHourFromTimestamp } from "@/lib/date";
 import { CoursWithClasse } from "@/services/db_services/supabase";
+import Link from "next/link";
 
 interface CoursListItemProps { // Définir une interface pour les props
     cours: CoursWithClasse; // Définir cours comme une prop
@@ -7,29 +8,33 @@ interface CoursListItemProps { // Définir une interface pour les props
 
 export default function CoursListItem({ cours }: CoursListItemProps) { // Déstructurer les props
     return (
-        <div className={`
-            border border-border_green
-            rounded 
-            flex flex-col
-            hover:shadow-md
-            bg-base_blanc
-        `}>
+        <Link
+            href={`/pointage?idCours=${cours.id_cours}`}
+        >
             <div className={`
-                py-2 px-4
-                flex justify-between flex-row items-center 
-                w-full
+                border border-border_green
+                rounded 
+                flex flex-col
+                hover:shadow-md
+                bg-base_blanc
             `}>
-                <span className="font-bold ml-4"> {cours.nom_cours}</span>
-                <span className="mr-4 font-bold"> {cours.classe?.nom_classe} </span>
+                <div className={`
+                    py-2 px-4
+                    flex justify-between flex-row items-center 
+                    w-full
+                `}>
+                    <span className="font-bold ml-4"> {cours.nom_cours}</span>
+                    <span className="mr-4 font-bold"> {cours.classe?.nom_classe} </span>
+                </div>
+                <div className={`
+                    py-2 px-4
+                    flex justify-between flex-row items-center 
+                    w-full
+                `}>
+                    <span className="text-text_grey ml-4"> {` ${getHourFromTimestamp(cours.horaire)}`}</span>
+                    <span className="mr-4 "> {cours.salle} </span>
+                </div>
             </div>
-            <div className={`
-                py-2 px-4
-                flex justify-between flex-row items-center 
-                w-full
-            `}>
-                <span className="text-text_grey ml-4"> {` ${getHourFromTimestamp(cours.horaire)}`}</span>
-                <span className="mr-4 "> {cours.salle} </span>
-            </div>
-        </div>
+        </Link>
     )
 }
