32cbae5b31e1ced9dbc6d4002ffbdebddeb67571
j2d6 <j2d6.pro@gmail.com>
2024-10-18 11:00:46 +0300

    make EleveIdentity a Link to dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function DashBoardPage() {
+
+   return (
+    <main>
+        DASHBOARD PAGE
+    </main>
+   )
+    
+}
--- components/pointage/eleveIdentityCard.tsx
+++ components/pointage/eleveIdentityCard.tsx
@@ -1,4 +1,5 @@
 import { Tables } from "@/services/db_services/supabase";
+import Link from "next/link";
 
 interface EleveIdentityCardProps {
     eleve: Tables<'eleve'>
@@ -7,15 +8,19 @@ interface EleveIdentityCardProps {
 export default function EleveIdentityCard({ eleve }: EleveIdentityCardProps) {
     
 
-    return (
+    return ( 
         <div className="flex flex-row items-center  flex-1 h-22   border-rose-600" >
-           <div className="flex flex-row items-center  border-green-500">
-                <img className="rounded-full w-11 h-11 " src={`/images/${eleve.id_eleve}.jpg`} alt="profile picture" />
-                <div className="space-y-0.5 text-xl font-bold dark:text-white text-left rtl:text-right ms-3">
-                    <div>{eleve.prenom}</div>
-                    <div className="text-sm text-gray-500 dark:text-gray-400 ">{eleve.id_eleve}</div>
+            <Link
+                href={`/dashboard?id_eleve=${eleve.id_eleve}`}
+            >    
+                <div className="flex flex-row items-center  border-green-500">
+                        <img className="rounded-full w-11 h-11 " src={`/images/${eleve.id_eleve}.jpg`} alt="profile picture" />
+                        <div className="space-y-0.5 text-xl font-bold dark:text-white text-left rtl:text-right ms-3">
+                            <div>{eleve.prenom}</div>
+                            <div className="text-sm text-gray-500 dark:text-gray-400 ">{eleve.id_eleve}</div>
+                        </div>
                 </div>
-           </div>
+            </Link>
         </div>   
     )
 }
